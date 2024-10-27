905da82048194f2d145fa02212b913cd299e21db
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2024-10-27 07:00:50 +0600

    Vesion 1.0

---
+++ README.md
@@ -0,0 +1,41 @@
+
+
+## 🏗 Get started
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/maruf119459/My-eCommers.git
+```
+
+2. Create virtual enviroment:
+
+```bash
+virtualenv env
+source env/bin/activate # for Linux
+.\env_name\Scripts\activate # for Windows
+```
+
+3. Install all the dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+4. Change directory to `ecommerce`:
+
+```bash
+cd ecommerce
+```
+
+5. Run migrations:
+
+```bash
+py manage.py migrate
+```
+
+6. Run the server:
+
+```bash
+py manage.py runserver
+```
---
+++ ecommerce/products/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from cart.views import add_to_cart, remove_from_cart, CartView, decreaseCart
+from . views import home, ProductDetail, Home
+from products.api.views import *
+from cart.api.views import *
+
+app_name = 'mainapp'
+
+router = SimpleRouter()
+router.register('api/products', ProductAPIView)
+router.register('api/category', CategoryAPIView)
+router.register('api/cart', CartAPIView)
+router.register('api/order', OrderAPIView)
+
+urlpatterns = [
+    # path('', Home.as_view(), name='home'),
+    path('', include(router.urls)),
+    path('', home, name='home'),
+    path('p', Home.as_view(), name='home'),
+    path('product/<slug>/', ProductDetail.as_view(), name='product'),
+    path('cart/', CartView, name='cart-home'),
+    path('cart/<slug>', add_to_cart, name='cart'),
+    path('decrease-cart/<slug>', decreaseCart, name='decrease-cart'),
+    path('remove/<slug>', remove_from_cart, name='remove-cart'),
+]
